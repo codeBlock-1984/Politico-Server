c40e93a9feaa35b7dc6291595b334bc44437cabc
IceOny <emmanuelroic@gmail.com>
2019-01-31 03:16:52 +0100

    [ feature #163610253 ] build create new office endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const parties = require('./routes/parties');
+const offices = require('./routes/offices');
 
 const app = express();
 const port = 3000 || process.env.port;
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/v1/parties', parties);
+app.use('/api/v1/offices', offices);
 
 app.listen(port, () => {
   console.log(`Politico server listening on port ${port}!`);
--- routes/offices.js
+++ routes/offices.js
@@ -1,1 +1,9 @@
+import officeController from '../controllers/office-controller';
 
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/', officeController.createOffice);
+
+module.exports = router;
