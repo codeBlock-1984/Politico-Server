168a5e6613e01fa327d40e8f4a2244c4769c0b37
IceOny <emmanuelroic@gmail.com>
2019-01-31 03:03:59 +0100

    [ feature #163610212 ] build delete party by ID endpoint

--- routes/parties.js
+++ routes/parties.js
@@ -8,5 +8,6 @@ router.post('/', partyController.createParty);
 router.get('/', partyController.getParties);
 router.get('/:id', partyController.getParty);
 router.patch('/:id', partyController.editParty);
+router.delete('/:id', partyController.deleteParty);
 
 module.exports = router;
